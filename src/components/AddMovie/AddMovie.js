e8cbea281034e8903f080c1bbc6127ce5c35dc02
Alex Kim <alexkim251@yahoo.com>
2021-06-18 23:58:44 -0500

    half of base for addmovie component

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -1,7 +1,37 @@
-function AddMovie() {
+import { useDispatch } from 'react-redux';
+import React, {useState}from 'react';
+
+const AddMovie = () => {
+
+    const dispatch = useDispatch();
+
+    let [newMovie, setMovie] = useState({
+        id: 15,
+        title: '',
+        poster: '',
+        description: ''
+    })
+
+    let [newMovieGenre, setNewmovieGenre] = useState({
+        
+    })
+
+    const handleNameChange = (event) => {
+        console.log('event happened');
+        setPlant({...newMovie, title: event.target.value})
+    }
+
+    const addNewMovie = (event) => {
+        event.preventDefault();
+        dispatch({ type: 'ADD_MOVIE', payload: newMovie})
+    }
+
     return
-        <>
-        </>
-}
+        <div>
+            <form onSubmit={addNewMovie}>
+                <input type='text' value={newMoive.title} onChange={handleNameChange} />
+                <input type='submit' value='Add New Movie' />
+            </form>
+        </div>
 
 export default AddMovie;
